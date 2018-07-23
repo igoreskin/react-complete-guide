a69dd9d4b25a071625f6d3da06e56fe8de6f63db
Igor Eskin <eskinhd@gmail.com>
2018-07-23 12:52:14 -0400

    Dynamic content added to new file Person.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I am a React App</h1> 
+        <h1>Hi, I&apos;m a React App</h1>
+        <p>This is really working!</p>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
+        <Person name="Stephanie" age="26"/>
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = (props) => {
+  return <p>I&apos;m {props.name} and I am {props.age} years old!</p>
+}
+
+export default person;
