18cb566e434c4b8322de2dad644f88ef9ddec547
Igor Eskin <eskinhd@gmail.com>
2018-07-27 11:28:29 -0400

    Rendering a list by mapping the state in App.js

--- src/App.js
+++ src/App.js
@@ -13,14 +13,6 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = (newName) => {
-    this.setState ({persons: [
-      {name: newName, age: 28},
-      {name: 'Manu', age: 29},
-      {name: 'Stephanie', age: 27}
-    ]
-    })
-  }
 
   nameChangeHandler = (event) => {
     this.setState ({persons: [
@@ -30,6 +22,12 @@ class App extends Component {
     ],
     showPersons: false,
     })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
   }
 
   togglePersonsHandler = () => {
@@ -48,15 +46,11 @@ class App extends Component {
 
     let persons = null;
     if (this.state.showPersons) {
-      persons =(
-        <div>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Max!')}
-            changed={this.nameChangeHandler}>My Hobbies: Racing</Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      persons = (
+        <div> // this is how a list is rendered: 
+          {this.state.persons.map((person, index) => {
+            return <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age} />
+          })}
         </div>
       )
     }
