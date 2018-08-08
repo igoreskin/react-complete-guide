4d5a3846df27ddc9ca98076b82a912b837183412
Igor Eskin <eskinhd@gmail.com>
2018-08-08 13:43:41 -0400

    Classes assigned dynamically in App.js for dynamic styling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
       {id: 'asfa1', name: 'Max', age: 28},
       {id: 'vasdf1', name: 'Manu', age: 29},
       {id: 'asdf11', name: 'Stephanie', age: 26}
-    ]
+    ],
+    showPersons: false
   }
 
 
@@ -20,7 +21,7 @@ class App extends Component {
     })
     const person = {...this.state.persons[personIndex]}; // using the spread operator to create a copy of a person object, without mutating it
     person.name = event.target.value;
-    const persons = [...this.state.persons];
+    const persons = [...this.state.persons]; // this is to create a copy of persons, i.e. without mutating the state
     persons[personIndex] = person;
     this.setState ({persons: persons,
     // showPersons: false,
@@ -40,7 +41,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -60,13 +62,23 @@ class App extends Component {
             changed={(event) => this.nameChangeHandler(event, person.id)} />
           })}
         </div>
-      )
+      );
+      style.backgroundColor = 'red';
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>Hi, I&apos;m a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
 
         <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
 
