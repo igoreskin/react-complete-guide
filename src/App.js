4ec16a0fdf0040ee2697c6f1250352cf5ea6c2c3
Igor Eskin <eskinhd@gmail.com>
2018-08-15 12:27:51 -0400

    Radium and StyleRoot removed from App.js and Person.js in preparation for CSS modules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Person from './Person/Person'
-import Radium, { StyleRoot} from 'radium';
+import Person from './Person/Person';
 
 class App extends Component {
 
@@ -47,10 +46,6 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black',
-      }
     }
 
     let persons = null;
@@ -68,10 +63,6 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black',
-      }
     }
 
     const classes = [];
@@ -84,19 +75,17 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi, I&apos;m a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
+      <div className="App">
+        <h1>Hi, I&apos;m a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
 
-          <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
 
-          {persons}
+        {persons}
 
-        </div>
-      </StyleRoot>
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import './Person.css';
-import Radium from 'radium';
 
 const person = (props) => {
 
-  const style = {
-    '@media (min-width: 500px)': {
-      width: '450px'
-    }
-  };
+  // const style = {
+  //   '@media (min-width: 500px)': {
+  //     width: '450px'
+  //   }
+  // };
 
 
   return (
-    <div className="Person" style={style}>
+    <div className="Person">
       <p onClick={props.click}>I&apos;m {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
@@ -20,4 +19,4 @@ const person = (props) => {
   )
 }
 
-export default Radium(person);
+export default person;
