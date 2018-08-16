9d235c30ed8632024aa39350bafb6d9ea3a22ef1
Igor Eskin <eskinhd@gmail.com>
2018-08-16 18:26:24 -0400

    Persons rendering logic moved to Persons.js stateless component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import classes from './App.css';
-import Person from '../components/Persons/Person/Person';
-import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
+import Persons from '../components/Persons/Persons';
 
 class App extends Component {
 
@@ -46,15 +45,10 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}>
-              <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              changed={(event) => this.nameChangeHandler(event, person.id)} />
-            </ErrorBoundary>
-          })}
+          <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangeHandler} />
         </div>
       );
       btnClass = classes.Red;
