4a4bee99cbdad80d36ef7c415120075683e067ad
Igor Eskin <eskinhd@gmail.com>
2018-08-21 11:29:43 -0400

    Return of App.js and Person.js wrapped into a higher-order component WithClass.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends PureComponent {
   constructor(props) {
@@ -30,7 +31,7 @@ class App extends PureComponent {
   //   console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState)
   //   return nextState.persons !== this.state.persons || nextState.showPersons !== this.state.showPersons;
   // }
-  //  Replaced by PureComponent! 
+  //  Replaced by PureComponent!
 
   componentWillUpdate(nextProps, nextState) {
     console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState)
@@ -76,7 +77,8 @@ class App extends PureComponent {
     }
 
     return (
-      <div className={classes.App}>
+      // <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
@@ -84,7 +86,8 @@ class App extends PureComponent {
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}/>
         {persons}
-      </div>
+      </WithClass>
+      // </div>
     );
   }
 }
